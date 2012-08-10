d2fdf1fd00445df9ba600ec4d790c06e04e7b321
Hiemanshu Sharma <hiemanshu@gmail.com>
2012-08-10 12:09:48 +0530

    Everything in working state & almost final configs, etc

--- database.py
+++ database.py
@@ -6,30 +6,31 @@ class DBController:
 		self.db = sqlite3.connect(filename)
 
 	def initDB(self):
-		self.db.execute("CREATE TABLE commits (id PRIMARY KEY, repo_name, path_to_repo, latest_tag, latest_commit, latest_commit_dev)")
+		self.db.execute("DROP TABLE IF EXISTS commits")
+		self.db.execute("CREATE TABLE commits (id PRIMARY KEY, repo_name, latest_tag, latest_commit, latest_commit_dev)")
 		self.db.commit()
 
-	def putNew(self, repo_name, path_to_repo, latest_tag, latest_commit, latest_commit_dev):
-		self.db.execute("insert into commits values (NULL, ?, ?, ?, ?, ?, ?)", (repo_name, path_to_repo, latest_tag, latest_commit, latest_commit_dev))
+	def putNew(self, repo_name, latest_tag, latest_commit, latest_commit_dev):
+		self.db.execute("INSERT INTO commits VALUES (NULL, ?, ?, ?, ?)", (repo_name, latest_tag, latest_commit, latest_commit_dev))
 		self.db.commit()
 
 	def updateTag(self, repo_name, latest_tag):
-		self.db.execute("update commits set latest_tag = ? where repo_name = ?", (latest_tag, repo_name))
+		self.db.execute("UPDATE commits SET latest_tag = ? WHERE repo_name = ?", (latest_tag, repo_name))
 		self.db.commit()
 
 	def updateCommit(self, repo_name, latest_commit):
-		self.db.execute("update commits set latest_commit = ? where repo_name = ?", (latest_commit, repo_name))
+		self.db.execute("UPDATE commits SET latest_commit = ? WHERE repo_name = ?", (latest_commit, repo_name))
 		self.db.commit()
 
 	def updateDevCommit(self, repo_name, latest_commit_dev):
-		self.db.execute("update commits set latest_commit_dev = ? where repo_name = ?", (latest_commit_dev, repo_name))
+		self.db.execute("UPDATE commits SET latest_commit_dev = ? WHERE repo_name = ?", (latest_commit_dev, repo_name))
 		self.db.commit()
 
-	def get(self, id):
-		return self.db.execute("select * from commits where id = ?", (id,)).fetchone()
 
-	def getRepo(self, id):
-		return self.db.execute("select * from commits where repo_name = ?", (repo_name,)).fetchone()
+	def getRepo(self, repo_name):
+		cur = self.db.cursor()
+		cur.execute("select * from commits where repo_name = ?", (repo_name,)).fetchone()
+		return cur.fetchall()
 
 	def getAllRepo(self):
 		cur = self.db.cursor()
--- schema.sql
+++ schema.sql
@@ -1,7 +1,6 @@
 CREATE TABLE commits (
 	id PRIMARY KEY,
 	repo_name,
-	path_to_repo,
 	latest_tag,
 	latest_commit,
 	latest_commit_dev
